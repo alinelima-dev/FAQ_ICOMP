cd73652b4485c77d90e52f6b604e0f75bcec6dbf
Aline Lima <alinerl.eu30@gmail.com>
2025-07-05 18:36:52 -0400

    refactor: ajuste na UI dos cards de perguntas

--- frontend/src/components/CardPergunta.tsx
+++ frontend/src/components/CardPergunta.tsx
@@ -5,6 +5,7 @@ import {
   CardContent,
   Button,
   Typography,
+  Box,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
@@ -20,47 +21,55 @@ const CardPergunta: React.FC<CardPerguntaProps> = ({ id, title, content }) => {
   return (
     <Card
       sx={{
-        boxShadow: 3,
-        borderRadius: 2,
+        borderRadius: 4,
+        p: "5px",
         background: "linear-gradient(135deg, #6a11cb, #2575fc)",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        minHeight: 300,
-        height: "100%",
       }}
     >
-      <CardContent>
-        <Typography
-          variant="h6"
-          component="h2"
-          sx={{ fontWeight: "bold", color: "white" }}
-        >
-          {title}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="white"
-          sx={{
-            mt: 2,
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            display: "-webkit-box",
-            WebkitLineClamp: 4,
-            WebkitBoxOrient: "vertical",
-          }}
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-      </CardContent>
-      <CardActions sx={{ justifyContent: "flex-end", px: 2, pb: 2 }}>
-        <Button
-          size="small"
-          color="secondary"
-          onClick={() => navigate(`/pergunta/${id}`)}
-        >
-          Ver mais
-        </Button>
-      </CardActions>
+      <Box
+        sx={{
+          borderRadius: 2,
+          backgroundColor: "white",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          minHeight: 300,
+          boxShadow: 3,
+        }}
+      >
+        <CardContent>
+          <Typography
+            variant="h6"
+            component="h2"
+            sx={{ fontWeight: "bold", color: "#2575fc" }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="#2575fc"
+            sx={{
+              mt: 2,
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: 4,
+              WebkitBoxOrient: "vertical",
+            }}
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        </CardContent>
+        <CardActions sx={{ justifyContent: "flex-end", px: 2, pb: 2 }}>
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => navigate(`/pergunta/${id}`)}
+          >
+            Ver mais
+          </Button>
+        </CardActions>
+      </Box>
     </Card>
   );
 };
